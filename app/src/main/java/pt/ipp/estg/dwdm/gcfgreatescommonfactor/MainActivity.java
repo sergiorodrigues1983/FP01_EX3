7b80711b16aa0445bc0293a6a474d2d061f087b4
Sérgio <sergiorodrigues1983@hotmail.com>
2020-10-17 20:11:12 +0100

    Resolvido

--- app/src/main/java/pt/ipp/estg/dwdm/gcfgreatescommonfactor/MainActivity.java
+++ app/src/main/java/pt/ipp/estg/dwdm/gcfgreatescommonfactor/MainActivity.java
@@ -13,22 +13,54 @@ import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
-    ArrayList numbers = new ArrayList();
+
+   // ArrayList numbers = new ArrayList();
+    public static final String fNumber = "FIRST_NUMBER";
+    public static final String sNumber = "SECOND_NUMBER";
+    public static final String gCFactor = "RESULT";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        EditText firstNumber = (EditText) findViewById(R.id.first_number);
-        EditText secondNumber = (EditText) findViewById(R.id.second_number);
 
-        numbers.add(firstNumber);
-        numbers.add(secondNumber);
     }
 
     public void nActivity(View view) {
         Intent intent = new Intent(this, Results.class);
-        intent.putExtra("NUMBERS", numbers);
+        EditText editTextFNumber = (EditText) findViewById(R.id.first_number);
+        EditText editTextSNumber = (EditText) findViewById(R.id.second_number);
+        String numberOne = editTextFNumber.getText().toString();
+        String numberTwo = editTextSNumber.getText().toString();
+        String result = gcf(Integer.parseInt(numberOne), Integer.parseInt(numberTwo));
+
+        intent.putExtra(fNumber, numberOne);
+        intent.putExtra(sNumber, numberTwo);
+        intent.putExtra(gCFactor, result);
         startActivity(intent);
+
     }
+
+    public String gcf(int n1, int n2) {
+        int remainder;
+        int higher, smaller;
+
+        if(n1 > n2) {
+            higher = n1;
+            smaller = n2;
+        } else {
+            higher = n2;
+            smaller = n1;
+        }
+
+        remainder = higher % smaller;
+
+        if (remainder != 0) {
+            return gcf(smaller, remainder);
+        }
+
+        return Integer.toString(smaller);
+
+    }
+
 }
--- app/src/main/java/pt/ipp/estg/dwdm/gcfgreatescommonfactor/Results.java
+++ app/src/main/java/pt/ipp/estg/dwdm/gcfgreatescommonfactor/Results.java
@@ -1,16 +1,29 @@
 package pt.ipp.estg.dwdm.gcfgreatescommonfactor;
 
-import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
-import java.util.ArrayList;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
 
 public class Results extends AppCompatActivity {
 
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        getIntent().getIntegerArrayListExtra("NUMBERS");
+        setContentView(R.layout.activity_results);
+
+        Intent intent = getIntent();
+        String numberOne = intent.getStringExtra(MainActivity.fNumber);
+        String numberTwo = intent.getStringExtra(MainActivity.sNumber);
+        String result = intent.getStringExtra(MainActivity.gCFactor);
+
+        TextView textViewOne = findViewById(R.id.fNumber);
+        TextView textViewTwo = findViewById(R.id.sNumber);
+        TextView textViewResult = findViewById(R.id.result);
+
+        textViewOne.setText(numberOne);
+        textViewTwo.setText(numberTwo);
+        textViewResult.setText(result);
     }
-
-
 }
